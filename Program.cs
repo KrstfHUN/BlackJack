c5e6357a192b364c6bf808400d4ab8e680ff9e3a
tothk <tothk@KRISTOF>
2024-11-12 08:17:35 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace BlackJack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BlackJack();
+        }
+
+        static void BlackJack()
+        {
+
+        }
+    }
+}
